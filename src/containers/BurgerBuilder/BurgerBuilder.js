1d1ee7b81244366138eb9c16f766f750a724375b
Nancy <nancyl1@student.unimelb.edu.au>
2020-04-16 14:26:22 +1000

    Allow dynamic ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
         }
     }
 
@@ -17,7 +17,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
   
             </Aux>
         );
