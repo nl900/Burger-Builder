c2749679249717ebec1c5ae38b03c5d88d593e3e
Nancy <nancyl1@student.unimelb.edu.au>
2020-04-18 10:51:27 +1000

    Add sidedrawer for mobile devices

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css'
+
+const sideDrawer = (props) => {
+    //conditionally attach different css classes
+    
+    return (
+        <div className={classes.sideDrawer}>
+        <Logo />
+        <nav>
+            <NavigationItems/>
+        </nav>
+        
+        </div>
+    );
+
+};
+
+export default sideDrawer;
