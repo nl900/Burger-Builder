82f3e93d21648714f610b7f15e02edf2f78733aa
Nancy <nancyl1@student.unimelb.edu.au>
2020-04-18 11:00:09 +1000

    Fix logo dimensions

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assets/images/burger.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger"/>
     </div>
 );
