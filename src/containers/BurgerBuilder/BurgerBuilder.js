a171f11c55406de88fc32bbf43c71041da4e02a3
Nancy <nancyl1@student.unimelb.edu.au>
2020-04-28 18:01:00 +1000

    fetch orders from server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://firebaseio.com/ingredients.json')
+        axios.get('/ingredients.json')
         .then(response => {
                 this.setState({ ingredients: response.data });
 
