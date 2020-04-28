2d5b40b7fdd1edd822ce0051ecb21c0ed8ad00ff
Nancy <nancyl1@student.unimelb.edu.au>
2020-04-28 17:42:19 +1000

    add navigation link to orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,7 @@ import classes from './Order.module.css';
 const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>$5</strong>></p>
+        <p>Price: <strong>$5</strong></p>
     </div>
 );
 
