57e6c187e2c9567aebc57801f550ae47df35da39
Nancy <nancyl1@student.unimelb.edu.au>
2020-05-06 17:14:41 +1000

    form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -57,9 +57,14 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault(); //prevent reload page
         this.setState({ loading: true });
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
 
         }
 
